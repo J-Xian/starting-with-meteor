6597a154b5d7b76d6064740883879aff1c447f81
J-Xian <87232219+J-Xian@users.noreply.github.com>
2022-09-21 13:08:32 +0800

    New update

--- meteor-wallet/imports/ui/App.jsx
+++ meteor-wallet/imports/ui/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { ContactForm } from './ContactForm';
 import { ContactList } from './ContactList';
+import { Header } from './Header';
 
 // Contact: name, email, imageURL
 export const App = () => (
   <div>
-    <ContactForm />
-    <ContactList />
+    <Header />
+    <div className='min-h-full'>
+      <div className='max-w-4xl mx-auto p-2'>
+        <ContactForm />
+        <ContactList />
+      </div>
+    </div>
+    
   </div>
 );
